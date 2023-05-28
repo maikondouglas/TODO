49e54acd125b9b8e08683a20827bb75e0e70d67a
Maikon Douglas <maikon.douglas@vindi.com.br>
2023-05-28 19:11:22 -0300

    add symbols and colors for icons

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,34 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
-  validates_presence_of :description
+  validates_presence_of :description, :due_date
   validates_inclusion_of :done, in: [true, false]
+
+  def symbol
+    symbols = {
+      'pending' => '»',
+      'done' => '✓',
+      'expired' => '✗'
+    }
+
+    symbols[status]
+  end
+
+  def css_color
+    colors = {
+      'pending' => 'primary',
+      'done' => 'success',
+      'expired' => 'danger'
+    }
+
+    colors[status]
+  end
+
+  private
+
+  def status
+    return 'done' if done?
+
+    due_date.past? ? 'expired' : 'pending'
+  end
 end
